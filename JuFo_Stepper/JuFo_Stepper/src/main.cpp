a2fa08d383f5e003e35e3a919ddf8ec54922d051
Suebaen <54914390+Suebaen@users.noreply.github.com>
2020-10-25 21:07:26 +0100

    Add files via upload

---
+++ JuFo_Stepper/JuFo_Stepper/src/main.cpp
@@ -0,0 +1,89 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <Stepper.h>
+
+
+//Servo myservo;
+
+SoftwareSerial serial_connection(2, 3);
+#define BUFFER_SIZE 64
+const int stepsPerRevolution = 200000000000;
+Stepper Stepper1(stepsPerRevolution, 4, 5, 6, 7);
+char inData[BUFFER_SIZE];
+char inChar=-1;
+int count=0;
+int i = 0;
+int winkel = 90;
+int warten = 10;
+int UmWievielSichDerServoDrehenSOll = 200;
+int first_bytes;
+int remainf_bytes=0;
+byte byte_count;
+int stepCount = 0; 
+int steps = 3;
+
+void setup() {
+  Stepper1.setSpeed(60);
+  Serial.begin(9600);//9600);
+  serial_connection.begin(9600);//9600);
+  serial_connection.println("Ready!!");
+  Serial.println("Started");
+  //myservo.attach(9);
+
+  }
+
+void loop() {
+    byte_count=serial_connection.available();
+   
+    if (byte_count){
+      Serial.println("Incoming Data");
+      first_bytes=byte_count;
+      remainf_bytes=0;
+      if (first_bytes>=BUFFER_SIZE-1){
+        remainf_bytes=byte_count-(BUFFER_SIZE-1);
+      }
+      
+      for (i = 0; i < first_bytes; i++){
+        inChar=serial_connection.read();
+        inData[i]=inChar;
+       Serial.println(first_bytes); 
+      }
+      inData[i]='\0';
+      
+      if (String(inData)=="N"){
+        Serial.println(inData);
+        Stepper1.step(steps);
+        Serial.print("steps:");
+        Serial.println(stepCount);
+        Serial.println("clockwise");
+        Stepper1.step(stepsPerRevolution);
+        stepCount++;
+        delay(warten);
+      }
+      if (String(inData)=="F"){
+        Serial.println(inData);
+        Stepper1.step(steps);
+        Serial.print("steps:");
+        Serial.println("counterclockwise");
+        Stepper1.step(-stepsPerRevolution);
+        stepCount--;
+        delay(warten);
+      }
+      
+      if (String(inData)=="FFFFFFFFF"){
+        Serial.println(inData);
+        Stepper1.step(steps);
+        Serial.print("steps:");
+        Serial.println("counterclockwise");
+        Stepper1.step(-stepsPerRevolution);
+        stepCount--;
+        delay(warten);
+      }
+      for ( i = 0; i < remainf_bytes; i++){
+        inChar=serial_connection.read();
+      }
+      
+      Serial.println(inData);
+      Serial.println(inData);
+    }
+}
